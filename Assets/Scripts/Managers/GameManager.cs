2ea9f856874e2545e8bf148d3849a4c4fbfd8246
rnrgll <shuajjjjj@naver.com>
2025-01-21 21:12:35 +0900

    Fix : 오르막길 수정(0-2, 0-3 씬)

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -23,19 +23,21 @@ public class GameManager
     public void Init()
     {
         
-        Debug.Log("GameManager Init 호출!!!!!!!!!!!!!!!1");
+        Debug.Log("GameManager Init 호출");
         settingData = new SettingData();
         
         //1. 저장된 Setting 파일이 있는지 확인한다.
         //있으면 로드
         if (Managers.Data.HasSaveDataFile(Define.SaveKey.SettingData))
         {
+            Debug.Log("<color=red>로컬에 저장된 데이터를 가져옵니다.</color>");
             Managers.Game.settingData = Managers.Data.LoadData<SettingData>(Define.SaveKey.SettingData);
         }
          
         //2. 없으면 초기화 파일 로드
         else
         {
+            Debug.Log("<color=red>로컬에 저장된 데이터가 없습니다. 파일을 초기화합니다.</color>");
             Managers.Game.settingData = Managers.Data.LoadInitData<SettingData>(Define.SaveKey.SettingData);
             
             //초기화 후 저장 경로에 해당 데이터를 저장한다.
