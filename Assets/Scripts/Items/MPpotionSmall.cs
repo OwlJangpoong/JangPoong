c8e4595a035d7d18196d7bf80d65294c74e51228
TIME0227 <newspaper0227@gmail.com>
2025-01-25 22:26:51 +0900

    Add : 아이템 획득 효과음, 회복 아이템 사용 시 자막, 레벨업 토큰 획득 시 자막 추가

--- Assets/Scripts/Items/MPpotionSmall.cs
+++ Assets/Scripts/Items/MPpotionSmall.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class MPpotionSmall : ItemBase
 {
+    public AudioClip audioClip;
     public override void UpdateCollision(Transform target)
     {
+        Managers.Sound.Play(audioClip);
+
         var playerDataManager = target.GetComponent<PlayerDataManager>();
 
         if (playerDataManager != null)
